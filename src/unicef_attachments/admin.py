87c1989752376b4ab36254ef5936d62a706c5b11
Greg Reinbach <greg@reinbach.com>
2019-03-12 06:30:19 -0400

    flake8 cleanup

--- src/unicef_attachments/admin.py
+++ src/unicef_attachments/admin.py
@@ -7,7 +7,7 @@ from unicef_attachments import models as app_models
 
 @admin.register(app_models.FileType)
 class FileTypeAdmin(OrderedModelAdmin):
-    list_display = ['label', 'name', 'code', 'move_up_down_links',]
+    list_display = ['label', 'name', 'code', 'move_up_down_links']
     list_filter = ['code', ]
     search_fields = ['name', 'label']
 
